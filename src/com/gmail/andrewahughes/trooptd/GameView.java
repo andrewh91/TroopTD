384c4cd9985612df44f138e7c6929ccbb5f6b3f1
andrewh91 <andrewahughes@googlemail.com>
2014-11-28 23:33:40 +0000

    started game view
    added gameview to r.id in xml

---
+++ src/com/gmail/andrewahughes/trooptd/GameView.java
@@ -0,0 +1,12 @@
+package com.gmail.andrewahughes.trooptd;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View{//make our GameView inherit from the View java class
+	public GameView(Context context, AttributeSet attributes){
+		super(context,attributes);//invoke the super class (View) 's constructor
+	}
+
+}
--- src/com/gmail/andrewahughes/trooptd/MainActivity.java
+++ src/com/gmail/andrewahughes/trooptd/MainActivity.java
@@ -2,17 +2,20 @@ package com.gmail.andrewahughes.trooptd;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+	
+	private GameView gameView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ImageView image = (ImageView) findViewById(R.id.ic_launcher);
-        
+		gameView = (GameView) findViewById(R.id.gameView);//need to add the gameView to r.id in xml
 	}
 
 	@Override
